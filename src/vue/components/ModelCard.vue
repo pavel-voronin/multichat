b7fe1e4f98893c9877d1383a1e818d493708af42
Pavel Voronin <pavel.a.voronin@gmail.com>
2026-03-23 19:26:16 +0800

    feat: add readonly prop to ModelCard to hide Change button

--- src/vue/components/ModelCard.vue
+++ src/vue/components/ModelCard.vue
@@ -10,7 +10,7 @@
       <span v-if="priceLabel" class="model-card-meta">{{ priceLabel }}</span>
       <span v-if="isFree" class="model-card-badge-free">Free</span>
     </div>
-    <UiButton size="sm" @click="$emit('change')">Change</UiButton>
+    <UiButton v-if="!readonly" size="sm" @click="$emit('change')">Change</UiButton>
   </div>
 </template>
 
@@ -23,6 +23,7 @@ import UiButton from './ui/UiButton.vue';
 const props = defineProps<{
   modelId: string;
   snapshot?: ModelSnapshot;
+  readonly?: boolean;
 }>();
 
 defineEmits<{ change: [] }>();
